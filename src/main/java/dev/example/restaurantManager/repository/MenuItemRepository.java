2c350519a373f1e7e9907ee3626ab9fa1d2c3ac1
AlexKano10 <alex40806@gmail.com>
2024-11-06 12:59:36 +0100

    Updated MenuItemRepository to support MainCourse and Dessert querying

--- src/main/java/dev/example/restaurantManager/repository/MenuItemRepository.java
+++ src/main/java/dev/example/restaurantManager/repository/MenuItemRepository.java
@@ -1,7 +1,21 @@
 package dev.example.restaurantManager.repository;
 
+import dev.example.restaurantManager.model.Dessert;
+import dev.example.restaurantManager.model.MainCourse;
 import dev.example.restaurantManager.model.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MenuItemRepository extends JpaRepository<MenuItem, String> {
+
+    // Búsqueda solo de main courses
+    @Query("SELECT m FROM MainCourse m")
+    List<MainCourse> findAllMainCourses();
+
+    // Búsqueda solo de desserts
+    @Query("SELECT d FROM Dessert d")
+    List<Dessert> findAllDesserts();
+
 }
