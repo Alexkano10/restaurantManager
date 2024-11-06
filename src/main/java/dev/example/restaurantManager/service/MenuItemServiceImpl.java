7c3e10589b4dc174c5bedbb6c06addab3d36d822
AlexKano10 <alex40806@gmail.com>
2024-11-06 13:08:39 +0100

    Update service layer to handle CRUD operations for MainCourse and Dessert subclasses

--- src/main/java/dev/example/restaurantManager/service/MenuItemServiceImpl.java
+++ src/main/java/dev/example/restaurantManager/service/MenuItemServiceImpl.java
@@ -1,5 +1,7 @@
 package dev.example.restaurantManager.service;
 
+import dev.example.restaurantManager.model.Dessert;
+import dev.example.restaurantManager.model.MainCourse;
 import dev.example.restaurantManager.model.MenuItem;
 import dev.example.restaurantManager.repository.MenuItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,22 +15,39 @@ public class MenuItemServiceImpl implements MenuItemService {
     @Autowired
     private MenuItemRepository menuItemRepository;
 
+    //Retrieve all MenuItems including subtypes
     @Override
     public List<MenuItem> getAllMenuItems() {
         return menuItemRepository.findAll();
     }
 
+    //Save a generic Menu item
     @Override
     public MenuItem createMenuItem(MenuItem menuItem) {
         menuItem.setId(UUID.randomUUID().toString());
         return menuItemRepository.save(menuItem);
     }
 
+    //Save a MainCourse
+    public MainCourse createMainCourse(MainCourse mainCourse){
+        mainCourse.setId(UUID.randomUUID().toString());
+        return menuItemRepository.save(mainCourse);
+    }
+
+    //Save Dessert
+
+    public Dessert createDessert(Dessert dessert){
+        dessert.setId(UUID.randomUUID().toString());
+        return menuItemRepository.save(dessert);
+    }
+
+    //Retrieve a MenuItem by ID
     @Override
     public MenuItem getMenuItemById(String id) {
         return menuItemRepository.findById(id).orElse(null);
     }
 
+    //Update an existing MenuItem
     @Override
     public MenuItem updateMenuItem(String id, MenuItem menuItemDetails) {
         MenuItem menuItem = menuItemRepository.findById(id).orElse(null);
